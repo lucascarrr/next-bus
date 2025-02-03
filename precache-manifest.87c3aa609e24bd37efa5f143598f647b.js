290315cbda83252fe2f2d0311e285ef80e6e8058
lucascarrr <lukegabrielcarr@gmail.com>
2025-02-03 22:21:18 +0200

    Updates

---
+++ precache-manifest.87c3aa609e24bd37efa5f143598f647b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56add205b18e4683888c8d748477cca9",
+    "url": "/next-bus/index.html"
+  },
+  {
+    "revision": "16674b3ed66d0f43ab8b",
+    "url": "/next-bus/static/css/main.7a701b3b.chunk.css"
+  },
+  {
+    "revision": "1a052530e938c881090a",
+    "url": "/next-bus/static/js/2.333fef68.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/next-bus/static/js/2.333fef68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6149019b11489b7b868",
+    "url": "/next-bus/static/js/3.dde89952.chunk.js"
+  },
+  {
+    "revision": "16674b3ed66d0f43ab8b",
+    "url": "/next-bus/static/js/main.0c2b627d.chunk.js"
+  },
+  {
+    "revision": "fe7a3722f29615bf97de",
+    "url": "/next-bus/static/js/runtime-main.0a120e8a.js"
+  }
+]);
